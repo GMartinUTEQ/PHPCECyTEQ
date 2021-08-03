e9e7e1f9cd724dd72883968056bcf70c1bcaf176
GMartin <gabosoft@gmail.com>
2021-08-03 11:57:26 -0500

    actividad6

--- Actividad006/index.php
+++ Actividad006/index.php
@@ -16,7 +16,42 @@
         <div class="card">
             <div class="card-header">Ejemplo:</div>
             <div class="card-body">
-
+                <?php
+                    $dia = 24; //Variable de tipo entero.
+                    $salario = 700.50; //Variable tipo decimal.
+                    $nombre = "Heidy"; //Variable de tipo string.
+                    $activo = true; //Variable de tipo Booleano.
+                    echo "El día de hoy es: " . $dia . "<br/>";
+                    echo "<hr/>"; //Horizontal Rule
+                    echo "Tu nombre es: " . $nombre . "<br/>";
+                    echo "Tu salario es: $" . money_format("%(#10n", $salario) . "<br/>";
+                    echo "Eres un empleado activo: " . $activo;
+                ?>
+            </div>
+            <div class="card-footer">Desarrollado por: GMartin</div>
+          </div>
+            <br/>
+          <div class="card">
+            <div class="card-header">Juego: Adivina Inferior, superior o medio.<a href="index.php">Recargar</a></div>
+            <div class="card-body">
+                <?php
+                    $sorteado = rand(1 ,20);
+                    if($sorteado == 10)
+                    {
+                        echo "<h1>El número es medio, es 10</h1>";
+                    }
+                    else
+                    {
+                        if($sorteado < 10)
+                        {
+                            echo "<h1>El número es inferior, es $sorteado</h1>";
+                        }
+                        else
+                        {
+                            echo "<h1>El número es superior, es " . $sorteado ."</h1>";
+                        }
+                    }
+                ?>
             </div>
             <div class="card-footer">Desarrollado por: GMartin</div>
           </div>
