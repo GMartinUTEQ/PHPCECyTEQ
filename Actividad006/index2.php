ef7c313ce0c2dc50ca096ce693774264129a4b3b
GMartin <gabosoft@gmail.com>
2021-08-05 10:16:18 -0500

    index

---
+++ Actividad006/index2.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Variables PHP</title>
+        <meta charset="utf-8" >
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+            <h1>Uso de variables en PHP</h1>
+            <p>Como trabajar con diferentes tipos de variables.</p>
+        </div>
+
+        <div class="card">
+            <div class="card-header">Ejemplo:</div>
+            <div class="card-body">
+            </div>
+            <div class="card-footer">Desarrollado por: GMartin</div>
+        </div>
+        <br/>
+    </body>
+</html>
