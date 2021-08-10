cf2ef815db94152a0d86571fc6b4abe57fd47cfb
GMartin <gabosoft@gmail.com>
2021-08-10 09:18:38 -0500

    act007

---
+++ Actividad007/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Uso de tablas HTML</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+<div class="container">
+  <h2>Ejemplo de tablas en HTML</h2>
+  <div class="card">
+    <div class="card-header">Este es un ejemplo de una tabla</div>
+    <div class="card-body">Content</div> 
+  </div>
+</div>
+
+</body>
+</html>
