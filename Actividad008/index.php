a9bbf5022800516804899b2f8bfdbcc93e95f7a5
GMartin <gabosoft@gmail.com>
2021-08-12 09:22:03 -0500

    func1

---
+++ Actividad008/index.php
@@ -0,0 +1,16 @@
+<html>
+
+    <body>
+        <?php
+
+            function mostrartitulo()
+            {
+                echo "<h1>Este es el título</h1>";
+            }
+
+            mostrartitulo();
+            echo "<br>";
+        ?>
+        <p>El título fue creado con una función</p>
+    </body>
+</html>
