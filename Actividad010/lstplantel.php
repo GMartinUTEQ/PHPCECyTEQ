33aca0f0940aaa69d0a9bddd1434329022fa49eb
GMartin <gabosoft@gmail.com>
2021-08-24 10:24:01 -0500

    act10

--- Actividad010/lstplantel.php
+++ Actividad010/lstplantel.php
@@ -35,54 +35,40 @@
             </div>
             <div class="card">
                 <div class="card-body">
-                    <table>
+                    <table class="table table-striped">
                         <tr>
                             <th>ID Plantel</th>
                             <th>Nombre plantel</th>
                         </tr>
                         <?php
-                        //Declaración de variables para conectar a mi base de datos.
-                        $servidor = "localhost";  //ip privada  ipconfig = 192.168.1.116 <- Ejemplo
-                        $usuario = "root";
-                        $contra = "desarrollo"; /* XAMPP = "" --> MAMP = "root" */
-                        $base = "cecyteq2021";
+                            include("conexion.php");
 
-                        //Creación de la variable $conex y se crea en $conex una nueva instancia de la clase mysqli
-                        $conex = new mysqli($servidor, $usuario, $contra, $base);
+                            //Declaro la variable $cadena  y le asigno la sentencia que quiero ejecutar en la bd.
+                            $cadena = "select * from plantel";
 
-                        //Valido si $conex tuvo algún error al conectarse a la base de datos.
-                        if($conex->connect_error)
-                        {
-                            //En caso de algún error, termino el programa y le mando como parámetro el error que se mo
-                            die("Error al conectar: " . $conex->connect_error);
-                        }
+                            //Declaro la variable $resultado y le asigno lo que traiga la base de datos en la consulta que
+                            $resultado = $conex->query($cadena);
 
-                        //Declaro la variable $cadena  y le asigno la sentencia que quiero ejecutar en la bd.
-                        $cadena = "select * from plantel";
-
-                        //Declaro la variable $resultado y le asigno lo que traiga la base de datos en la consulta que
-                        $resultado = $conex->query($cadena);
-
-                        //Resultado trae un conjunto de datos (Como un arreglo.)
-                        if($resultado->num_rows > 0)
-                        {
-                            //Ciclo while para recorrer el conjunto de resultados fila por fila.
-                            while($fila = $resultado->fetch_assoc())
+                            //Resultado trae un conjunto de datos (Como un arreglo.)
+                            if($resultado->num_rows > 0)
                             {
-                                //Imprimo el idplantel concateno un  - e imprimo el nombre del plantel
-                                echo "<tr>
-                                        <td>" . $fila["idplantel"] . "</td>
-                                        <td>" . $fila["nombreplantel"] . "</td>
-                                    </tr>";
+                                //Ciclo while para recorrer el conjunto de resultados fila por fila.
+                                while($fila = $resultado->fetch_assoc())
+                                {
+                                    //Imprimo el idplantel concateno un  - e imprimo el nombre del plantel
+                                    echo "<tr>
+                                            <td>" . $fila["idplantel"] . "</td>
+                                            <td>" . $fila["nombreplantel"] . "</td>
+                                        </tr>";
+                                }
                             }
-                        }
-                        else
-                        {
-                            //En caso contrario que la consulta no traiga ninguna fila, mostraré un mensaje.
-                            echo "<h3>La consulta no trajo ningún registro</h3>";
-                        }
-                        //cierro mi conexión 
-                        $conex->close();
+                            else
+                            {
+                                //En caso contrario que la consulta no traiga ninguna fila, mostraré un mensaje.
+                                echo "<h3>La consulta no trajo ningún registro</h3>";
+                            }
+                            //cierro mi conexión 
+                            $conex->close();
 
 
 
